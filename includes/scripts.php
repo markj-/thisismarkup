52e168ea62b7d1c8100584ddef7e3ecdf72f4ba6
Mark Jones <mark@thisismarkup.co.uk>
2014-01-13 22:48:18 +0000

    Use new version of prettify

---
+++ includes/scripts.php
@@ -0,0 +1,9 @@
+    <script src="scripts/prettify.min.js"></script>
+    <script>
+      (function() {
+        var codeBlocks = document.getElementsByClassName( 'prettyprint' );
+        if ( codeBlocks.length ) {
+          prettyPrint();
+        }
+      })();
+    </script>
